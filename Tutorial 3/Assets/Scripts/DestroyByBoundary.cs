e1a945118155702db526dad3fbb977e03d2a0045
Katerina Samson <54958992+katerinasamson7@users.noreply.github.com>
2019-11-10 23:44:09 -0500

    Tutorial 3

---
+++ Tutorial 3/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Tutorial 3/Assets/Scripts/Destroy_By_Contact.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_By_Contact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
+        }
+
+        // Update is called once per frame
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
